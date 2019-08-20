b32c89ed102bd3c02d646dd86940b6033b8996f2
Jack <jackbarber1998@hotmail.com>
2019-08-20 14:54:52 +0100

    Add database queries

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-const { ApolloServer, gql } = require('apollo-server');
-const data = require("./snacks.json");
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema.js');
-const prisma = require("./generated/prisma-client");
+const prisma = require("./generated/prisma-client/");
 
-function getNutritional() {
-    return (
-        {
-            "id": 1,
-            "calories": 160.2,
-            "sugar": 16.5,            
-        }
-    );
+function getNutritional(databaseEntry) {
+    return ({
+        "calories": databaseEntry.nutrition().calories,
+        "sugar": databaseEntry.nutrition().sugar
+    });
 }
 
-function getDietary() {
+function getDietary(databaseEntry) {
     return ( 
         {
-            "id": 1,
-            "vegetarian": true,
-            "vegan": false,
-            "nutFree": true,
-            "glutenFree": false,
+            "vegetarian": databaseEntry.dietary().vegetarian,
+            "vegan": databaseEntry.dietary().vegan,
+            "nutFree": databaseEntry.dietary().nutFree,
+            "glutenFree": databaseEntry.dietary().glutenFree,
         }
     );
 }
 
-// A map of functions which return data for the schema.
 const resolvers = {
   Query: {
     snack: () => {
+        const databaseEntry = server.context().prisma.prisma.snack({ id: "cjzjpljikq27h0b53dqfr9u99" });
         const snackEntry = {
-            "id": 1,
-            "name": "Malteser",
-            "description": "hmm",
-            "nutrition": getNutritional(),
-            "dietary": getDietary(),
+            "id": databaseEntry.id,
+            "name": databaseEntry.name,
+            "description": databaseEntry.description,
+            "nutrition": getNutritional(databaseEntry),
+            "dietary": getDietary(databaseEntry),
         }
         return snackEntry;
     },
@@ -60,12 +55,14 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: request => ({
-      ...request,
-      prisma
-  }),
+  context: () => {
+    return {
+        "text": "hello world",
+        prisma,
+    }
+  },
 });
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`)
+    console.log(`🚀 Server ready at ${url}`);
 });
